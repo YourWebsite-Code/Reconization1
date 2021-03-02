45be4f411c6cf7c11a92bc1d8574a00fc1ce8a78
YourWebsite-Code <78820495+YourWebsite-Code@users.noreply.github.com>
2021-03-02 14:22:53 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ Webcam.attach('#camera');
 
 function take_snapshot() { Webcam.snap(function(data_uri) { document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>'; }); }
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/VyEjRiuKu/model.json', modelLoaded);
 
 function modelLoaded() { console.log('Model Loaded!'); }
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) { console.error(error); } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
